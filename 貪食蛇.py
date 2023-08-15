a28fdd0958dcca4cf233c7c4f0d0457c4462129d
weber <t8101349@gmail.com>
2023-08-16 02:44:40 +0800

    8/19
    8/19

--- 貪食蛇.py
+++ 貪食蛇.py
@@ -40,4 +40,29 @@ window = Tk()
 window.title("Snake game")
 window.resizable(False, False)
 
+score = 0
+direction = 'down'
+
+label = Label(window, text='Score:{}'.format(score), font=('consola', 40))
+label.pack()
+
+canvas = Canvas(window, bg=BACKGROUND_COLOR,
+                height=GAME_HEIGHT, width=GAME_WIDTH)
+canvas.pack()
+
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int(screen_width/2) - int(window_width/2)
+y = int(screen_height/2) - int(window_height/2)
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake = Snake()
+food = Food()
+
 window.mainloop()
