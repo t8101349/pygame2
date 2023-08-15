75b59cf92ac22949b8644bad2398904dd39cfc66
weber <t8101349@gmail.com>
2023-08-15 22:56:50 +0800

    create window

---
+++ 貪食蛇.py
@@ -0,0 +1,43 @@
+from tkinter import *
+import random
+
+
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PART = 3
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
+
+
+class Snake:
+    pass
+
+
+class Food:
+    pass
+
+
+def next_turn():
+    pass
+
+
+def change_direction(new_direction):
+    pass
+
+
+def check_collistion():
+    pass
+
+
+def game_over():
+    pass
+
+
+window = Tk()
+window.title("Snake game")
+window.resizable(False, False)
+
+window.mainloop()
